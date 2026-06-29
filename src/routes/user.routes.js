cfb413e01357e8c9b73bf0b8d0ab596c588dc9d1
Shristi Rai <shristirai.it@gmail.com>
2026-06-29 15:50:20 +0545

    separated controller modularly

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,45 +1,10 @@
 import express from 'express';
+import { getAll, getById, create, update, del } from '../controllers/user.conroller.js';
 
 const router = express.Router();
 
 const users = [];
-const getAll = (req, res) =>{
-    // res.send("<h1>Users Page</h1>");
-    // const query = req.query;
-    // console.log(query);
-    res.status(200).json({
-        message : "all users fetched",
-        success: true,
-        data: users,
-    });
-}
 
-const getById =  (req, res) =>{
-    // res.send("<h1>Users Page</h1>");
-    // req.params => {id: 123}
-    // console.log(req.params);
-    // /post/:userId/:postId => /post/1/2 {userId:1, postId:2}
-
-    const {id} = req.params;
-
-    const user = users.find((user)=> user._id === Number(id));
-
-    if(!user){
-        res.status(404).json({
-            message : `user not found `,
-            success: false,
-            data: null
-        });
-        return;
-    }
-    res.status(200).json({
-            message : `user fetched by {id} `,
-            success: true,
-            data: users
-        });
-
-
-};
 //! CRUD users
 //* get all users
 router.get("/", getAll);
@@ -54,79 +19,12 @@ router.get("/", getAll);
 router.get("/:id",getById);
 
 //* create
-router.post("/", (req, res) =>{
-    // res.send("<h1>Users created</h1>");
-    // console.log(req.body);
-
-    const {name, email, password} = req.body;
-    users.push({
-        name,
-        email,
-        password,
-        createdAt: Date.now(),
-        _id: users.length + 1 ,
-    })
-    res.status(201).json({
-        message : "user created",
-        success: true,
-        data: users[users.length-1]
-    });
-});
+router.post("/", create);
 
 //* update
-router.put("/:id", (req, res) =>{
-    // res.send("<h1>Users updated</h1>");
-
-    const {id} = req.params;
-
-    const {name, email, password} = req.body;
-    
-    const index = users.findIndex((user)=> user._id === Number(id));
-
-    if(index === -1){
-        res.status(404).json({
-            message: "user not found",
-            success: "false",
-            data: null
-        });
-        return;
-    }
-
-    users[index] = {
-        ...users[index],
-        name,
-        email,
-        password
-    };
-    res.status(200).json({
-        message : "user updated",
-        success: true,
-        data: users[index]
-    });
-});
+router.put("/:id",update);
 
 //* delete
-router.delete("/:id", (req, res) =>{
-    // res.send("<h1>Users deleted</h1>");
-
-    const id = req.params.id;
-
-    const index = users.findIndex((user)=> user._id === Number(id));
-
-    if(index === -1){
-        res.status(404).json({
-            message: "user not found",
-            success: false,
-            data: null
-        });
-        return;
-    }
-    users.splice(index, 1);
-    res.status(200).json({
-        message : "user deleted",
-        success: true,
-        data: null
-    });
-});
+router.delete("/:id", del);
 
 export default router;
